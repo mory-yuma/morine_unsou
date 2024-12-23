222c7ca24358322ef9d6924c512650a87202c03b
mory-yuma <129245023+mory-yuma@users.noreply.github.com>
2024-12-23 19:18:20 +0900

    採用情報ページのデザインを整えた

--- page-recruit.php
+++ page-recruit.php
@@ -1,8 +1,5 @@
 <?php get_header();?>
     <main>
-        <section class="recruit_fv">
-            
-        </section>
         <section class="job-sec wrapper">
             <div class="job-post driver">
                 <h3 class="job-title">運転手</h3>
@@ -13,16 +10,37 @@
                             <li>・港から各取引先へコンテナ横持ち</li>
                             <li>・港から各配送先へコンテナ配送（切り離し・手下ろし）</li>
                         </ul>
-                        <button class="must-skill">必要なスキル</button>
+                        <button class="must-skill">必要なスキル/勤務地</button>
                         <div class="modal" id="skillModal" style="display: none;">
                             <div class="modal-content">
                                 <span class="close-btn">&times;</span>
-                                <h4>必要なスキル</h4>
-                                <ul>
-                                    <li>・普通自動車運転免許（AT限定可）</li>
-                                    <li>・牽引免許</li>
-                                    <li>・フォークリフト運転技能者</li>
-                                </ul>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>項目</th>
+                                            <th>内容</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>必要なスキル</td>
+                                            <td>
+                                                <ul>
+                                                    <li>普通自動車運転免許（AT限定可）</li>
+                                                    <li>牽引免許</li>
+                                                    <li>フォークリフト運転技能者</li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>勤務地</td>
+                                            <td>
+                                                <p>沖縄県うるま市兼箇段１５４７－１</p>
+                                                <p>沖縄県那覇市港町２－４－１７</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -37,28 +55,41 @@
                             <li>・自社倉庫にてデバンニング作業（貨物をコンテナから取り出す作業）</li>
                             <li>・フォークリフトオペレーター</li>
                         </ul>
-                        <button class="must-skill">必要なスキル</button>
+                        <button class="must-skill">必要なスキル/勤務地</button>
                     </div>
                     <img src="https://placehold.jp/400x300.png" alt="倉庫の中の作業員の写真">
                     <div class="modal" id="skillModal" style="display: none;">
-                            <div class="modal-content">
-                                <span class="close-btn">&times;</span>
-                                <h4>必要なスキル</h4>
-                                <ul>
-                                    <li>・普通自動車運転免許（AT限定可）</li>
-                                    <li>・フォークリフト運転技能者 あれば尚可</li>
-                                    <li>・８トン限定中型自動車免許 あれば尚可</li>
-                                </ul>
-                            </div>
+                        <div class="modal-content">
+                            <span class="close-btn">&times;</span>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>項目</th>
+                                        <th>内容</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>必要なスキル</td>
+                                        <td>
+                                            <ul>
+                                                <li>普通自動車運転免許（AT限定可）</li>
+                                                <li>フォークリフト運転技能者 あれば尚可</li>
+                                                <li>８トン限定中型自動車免許 あれば尚可</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>勤務地</td>
+                                        <td>
+                                            <p>沖縄県沖縄市字知花４－３５－１３</p>
+                                            <p>沖縄県那覇市港町２－４－１７</p>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                </div>
-            </div>
-
-            <div class="work-place">
-                <h3>勤務地</h3>
-                <div class="">
-                    <div class="driver-place"></div>
-                    <div class="warehouse-place"></div>
+                    </div>
                 </div>
             </div>
         </section>
